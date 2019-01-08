8ff363b5d7c334db281c4bc29787872a2b1baa01
Nguyễn Đức Đại <46410386+dainguyennha@users.noreply.github.com>
2019-01-08 08:43:10 +0700

    duhoot

---
+++ js/app.js
@@ -0,0 +1,59 @@
+document.querySelector('#readmoreBtn').addEventListener('click', function () {
+	document.querySelector('#p1clicked').style.display = 'block';
+	document.querySelector('#readmoreBtn').style.display = 'none';
+
+});
+
+document.querySelector('#hideBtn').addEventListener('click', function () {
+	document.querySelector('#p1clicked').style.display = 'none';
+	document.querySelector('#readmoreBtn').style.display = 'inline';
+
+});
+
+document.querySelector('#readmoreBtn2').addEventListener('click', function () {
+	document.querySelector('#p1clicked2').style.display = 'block';
+	document.querySelector('#readmoreBtn2').style.display = 'none';
+
+});
+
+document.querySelector('#hideBtn2').addEventListener('click', function () {
+	document.querySelector('#p1clicked2').style.display = 'none';
+	document.querySelector('#readmoreBtn2').style.display = 'inline';
+
+});
+
+document.querySelector('#readmoreBtn3').addEventListener('click', function () {
+	document.querySelector('#p1clicked3').style.display = 'block';
+	document.querySelector('#readmoreBtn3').style.display = 'none';
+
+});
+
+document.querySelector('#hideBtn3').addEventListener('click', function () {
+	document.querySelector('#p1clicked3').style.display = 'none';
+	document.querySelector('#readmoreBtn3').style.display = 'inline';
+
+});
+
+
+
+
+document.querySelector('.loadmore').addEventListener('click', function () {
+	document.querySelector('.loadmore').style.display = 'none';
+	document.querySelector('.hid').style.display = 'block';
+
+});
+
+document.querySelector('#readmoreBtn4').addEventListener('click', function () {
+	document.querySelector('#p1clicked4').style.display = 'block';
+	document.querySelector('#readmoreBtn4').style.display = 'none';
+
+});
+
+document.querySelector('#hideBtn4').addEventListener('click', function () {
+	document.querySelector('#p1clicked4').style.display = 'none';
+	document.querySelector('#readmoreBtn4').style.display = 'inline';
+
+});
+
+
+var button = document.querySelector('selector');
